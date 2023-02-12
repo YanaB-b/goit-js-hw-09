400ae970c6d51b2c06ce9d9fc14447d9aa66e4d4
YanaB-b <j.biliuk96@gmail.com>
2023-02-12 21:12:33 +0100

    Виправлення

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,19 +2,69 @@
 import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
 
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Report } from 'notiflix/build/notiflix-report-aio';
 
-const elInput = document.querySelector("#datetime-picker");
-const valueEl = document.querySelector(".value");
-const labelEl = document.querySelector('button[data-start]');
-labelEl.disabled = true;
-let refsDate = null;
 
-function onValue (value) {
-    return String(value).padStart(2, '0');
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+  daretimePicker: document.querySelector('#datetime-picker'),
+
+};
+
+refs.startBtn.disabled = true;
+let refsDate = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      refsDate = selectedDates[0].getTime()
+      flatpickrClose();
+  },
+};
+flatpickr('#datetime-picker', options)
+
+refs.startBtn.addEventListener('click', timeStart)
+
+function flatpickrClose() {
+  if( refsDate < options.defaultDate.getTime()){
+    Notiflix.Notify.failure("Please choose a date in the future");
+
+}else {
+
+  refs.startBtn.disabled = false;
+    refs.daretimePicker.disabled = true;
+    Notify.success('Date is OK');
+  }
+}
+
+
+function timeStart(){
+  const onInterval = setInterval(() => {
+    refs.startBtn.disabled = true;
+    const intervalDate = Date.now();
+    const ms = refsDate - intervalDate;
+
+    const { days, hours, minutes, seconds } = convertMs(ms);
+    refs.days.textContent = `${addLeadingZero(days)}`;
+    refs.hours.textContent = `${addLeadingZero(hours)}`;
+    refs.minutes.textContent = `${addLeadingZero(minutes)}`; 
+    refs.seconds.textContent = `${addLeadingZero(seconds)}`;
+   
+    if(ms < 1000) {
+      clearInterval(onInterval);
+    }
+  },1000);
 
 }
 
@@ -26,77 +76,19 @@ function convertMs(ms) {
     const day = hour * 24;
   
     // Remaining days
-    const days = onValue(Math.floor(ms / day));
-    // Remaining hours
-    const hours = onValue(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes =onValue (Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = onValue(Math.floor((((ms % day) % hour) % minute) / second));
-  
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
     return { days, hours, minutes, seconds };
   }
   
 
-function onTime({ days, hours, minutes, seconds }) {
-  valueEl[0].textContent = `${days}`;
-  valueEl[1].textContent = `${hours}`;
-  valueEl[2].textContent = `${minutes}`; 
-  valueEl[3].textContent = `${seconds}`;
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
-
-  
-  const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        labelEl.disabled = false;
-        labelEl.style.background = '#00ffff';
-        refsDate = selectedDates[0];
-
-const onDefauitDate = options.defaultDate;
-if (onDefauitDate >  refsDate){
-    labelEl.disabled = true;
-    labelEl.style.background = 'green';
-
- Report.failure("Please choose a date in the future")
-}else {
-    Notify.success('Date is OK');
-
-}
-    },
-  };
-
-  flatpickr(elInput, options);
-
-  function timeStart() {
-    elInput.disabled = true;
-    labelEl.disabled = true;
-    labelEl.style.background = 'pink';
-    elInput.style.background = 'pink';
-  }  
-
-  let onInterval = null;
-  onInterval = setInterval(() => {
-    const intervalDate = new Date();
-    const valueDate =  refsDate - intervalDate;
-    if(valueDate < 1000) {
-clearInterval (onInterval);
-Report.success('Okey');
-elInput.disabled = false;
-elInput.style.background = '#00ffff';
-
-
-    }
-
-    let conver = convertMs(valueDate);
-    onTime(conver);
-
-  },1000);
-
-
-  labelEl.addEventListener ('click', () => {
-    timeStart();
-  });
